ca3327eae8a8f29e9a2762ee318b2375cad1a7ff
ParvezAhamed7 <parvezahamed700@gamil.com>
2025-09-07 17:29:24 +0530

    Initial commit for frontend

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const API_URL = 'https://student-backend-mm7t.onrender.com';
+export const API_URL = 'https://student-backend-otjm.onrender.com';
